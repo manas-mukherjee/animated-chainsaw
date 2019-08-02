e2408030fdcdc77619cd8322751248ab5b5d5e70
Manas Mukherjee <mmanas19@gmail.com>
2019-08-02 00:59:44 -0700

    added legend

--- js/main_d3.js
+++ js/main_d3.js
@@ -23,7 +23,11 @@ function getSlideSpecificColumnValue(d){
 	  	return val; 
 }
 
-updateSVG("EA");
+window.addEventListener('load', function() {
+    console.log('All assets are loaded');
+    updateSVG("EA");
+})
+
 
 function updateSVG(slide_name){
 	if (typeof window.slide_name == 'undefined') {
@@ -31,7 +35,12 @@ function updateSVG(slide_name){
 	}
 	
 
-	var margin = {top: 10, right: 20, bottom: 100, left: 80},
+	// var margin = {top: 10, right: 20, bottom: 100, left: 80},
+	//     width = 960 - margin.left - margin.right,
+	//     height = 500 - margin.top - margin.bottom;
+
+
+	var margin = {top: 10, right: 120, bottom: 100, left: 80},
 	    width = 960 - margin.left - margin.right,
 	    height = 500 - margin.top - margin.bottom;
 
@@ -123,6 +132,7 @@ function updateSVG(slide_name){
 	})
 
 
+
 	  svg.selectAll(".bar")
 	      .data(data)
 	    .enter().append("rect")
@@ -138,12 +148,15 @@ function updateSVG(slide_name){
 		    } else if (d.Zone === "2") {
 		      return "blue";
 		    } else if (d.Zone === "3"){
-		      return "green";
-		    } else if (d.Zone === "4"){
 		      return "orange";
+		    } else if (d.Zone === "4"){
+		      return "green";
 		    } else if(d.Zone === "5") {
 		      return "violet";
+		  	} else if(d.Zone === "6") {
+		      return "yellow";
 		  	}
+
 		  })
 	      .on('mouseover', tip.show)
 	      .on('mouseout', tip.hide)
@@ -212,18 +225,196 @@ function updateSVG(slide_name){
 	  svg.append("text")
 	      // .attr("transform", "rotate(-90)")
 	      .attr("y", function(d){return y_new(average_ridership_data(d))})
-	      .attr("x", width-75)
+	      .attr("x", width-60)
 	      .attr("dy", "1em")
 	      .style("text-anchor", "middle")
-	      .text("Avg. line(Across all stations)"); 
+	      .text("Avg commuters"); 
+
+		var annotations = [
+		  {
+		    "cx": 327,
+		    "cy": 200,
+		    "r": 20,
+		    "text": "A bit high number of commuters at this time. Reason is not known",
+		    "textWidth": 200,
+		    "textOffset": [
+		      0,
+		      -50
+		    ]
+		  },
+		  {
+		    "cx": 590,
+		    "cy": 220,
+		    "r": 20,
+		    "text": "Because of the International airport and university, this station is always busy",
+		    "textWidth": 200,
+		    "textOffset": [
+		      0,
+		      -50
+		    ]
+		  },
+		  {
+		    "cx": 430,
+		    "cy": 80,
+		    "r": 90,
+		    "text": "Many tech people stay at Zone3 and commute to the city",
+		    "textWidth": 200,
+		    "textOffset": [
+		      0,
+		      -50
+		    ]
+		  },
+		  {
+		    "cx": 590,
+		    "cy": 100,
+		    "r": 20,
+		    "text": "Because of the International airport and university, this station is always busy",
+		    "textWidth": 200,
+		    "textOffset": [
+		      0,
+		      -50
+		    ]
+		  },
+		  {
+		    "cx": 720,
+		    "cy": 200,
+		    "r": 50,
+		    "text": "This is the only time-segment where people commute from Gilroy to SFO(distance ~80 miles)",
+		    "textWidth": 200,
+		    "textOffset": [
+		      -15,
+		      -50
+		    ]
+		  },
+		  {
+		    "cx": 380,
+		    "cy": 150,
+		    "r": 25,
+		    "text": "University and major software companies keep it busy even in the mid-day.",
+		    "textWidth": 200,
+		    "textOffset": [
+		      0,
+		      -50
+		    ]
+		  },
+		  {
+		    "cx": 590,
+		    "cy": 230,
+		    "r": 20,
+		    "text": "Because of the International airport and university, this station is always busy",
+		    "textWidth": 200,
+		    "textOffset": [
+		      0,
+		      -50
+		    ]
+		  },
+		  {
+		    "cx": 430,
+		    "cy": 80,
+		    "r": 90,
+		    "text": "Many people from SFO, work for big companies like FB, Google, Intuit etc.",
+		    "textWidth": 200,
+		    "textOffset": [
+		      0,
+		      -50
+		    ]
+		  },
+		  {
+		    "cx": 590,
+		    "cy": 130,
+		    "r": 20,
+		    "text": "Because of the International airport and university, this station is always busy",
+		    "textWidth": 200,
+		    "textOffset": [
+		      0,
+		      -50
+		    ]
+		  },
+		  {
+		    "cx": 430,
+		    "cy": 120,
+		    "r": 60,
+		    "text": "Many people from SFO, work for big companies like FB, Google, Intuit etc.",
+		    "textWidth": 200,
+		    "textOffset": [
+		      -20,
+		      -50
+		    ]
+		  }
+		];
+
+	      
+	   var ringNote = d3.ringNote().draggable(false);
+
+	   svg.append("g")
+	     .attr("class", "annotations")
+	     .call(ringNote, function(d) { 
+	     	val = null;
+		  	if (slide_name === "EA"){
+		  		val = annotations.slice(0,2);
+		  	}else if (slide_name === "AM"){
+		  		val = annotations.slice(2,5);
+		  	}else if (slide_name === "MD"){
+		  		val = annotations.slice(5,7);
+		  	}else if (slide_name === "PM"){
+		  		val = annotations.slice(7,9);
+		  	}else if(slide_name === "EV"){
+		  		val = annotations.slice(9,11);
+		  	}
+	     	return val;
+	     });
+
+		   //  if (d.Zone === "1") {
+		   //    return "red";
+		   //  } else if (d.Zone === "2") {
+		   //    return "blue";
+		   //  } else if (d.Zone === "3"){
+		   //    return "orange";
+		   //  } else if (d.Zone === "4"){
+		   //    return "green";
+		   //  } else if(d.Zone === "5") {
+		   //    return "violet";
+		  	// } else if(d.Zone === "6") {
+		   //    return "yellow";
+
+		 color_array = ["red", "blue", "orange", "green", "violet", "Maroon"];  
+		 zone_array  = ["Zone-1","Zone-2","Zone-3","Zone-4","Zone-5","Zone-6"];
+
+	     //Draw the Rectangle
+	     var data = [0, 30, 60, 90, 120, 150];
+		 var rectangle = svg.append("g")
+                            .attr("transform", "translate("+15+","+0+")")
+                            .selectAll("rect")
+                            .data(data)
+                            .enter()
+                            .append("rect")
+		                            .attr("x", width)
+		                            .attr("y", function(d){return d})
+		                            .attr("width", 20)
+		                            .attr("height",20)
+		                            .attr("border", 1)
+		                            .attr("fill", function(d, i) {return color_array[i]; })
+
+		 var text = svg.append("g")
+                            .attr("transform", "translate("+60+","+15+")")
+                            .selectAll("text")
+                            .data(data)
+                            .enter()		                            
+		                    .append("text")
+		                    	  .attr("x", width+20+20 )
+							      .attr("y", function(d){return d})
+							      // .attr("dy", "0em")
+							      .style("text-anchor", "end")
+							      .text(function(d, i){return zone_array[i]});  
 
 	});
-
 }
 
 function type(d) {
   // d.AM = +d.AM;
   return d;
 }
+
+
 
 
